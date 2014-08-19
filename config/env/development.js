2d3aac45c5463760c0f8e7caab508e32f4351e0c
gmt <generalgmt@yahoo.com>
2014-08-19 22:07:16 +0100

    title changed

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@
 module.exports = {
 	db: 'mongodb://localhost/mean-dev',
 	app: {
-		title: 'MEAN - Development Environment'
+		title: 'Forum Demo app'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
